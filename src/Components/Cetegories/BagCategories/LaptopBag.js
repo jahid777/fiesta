50740802f42384ac1913efa3716480055b9279cf
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2021-11-11 14:58:29 +0600

    products images setup

--- src/Components/Cetegories/BagCategories/LaptopBag.js
+++ src/Components/Cetegories/BagCategories/LaptopBag.js
@@ -1,29 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { laptopBag } from "../../../AllData/BagData";
 
 const LaptopBag = () => {
+  const [laptopBags, setLaptopBag] = useState(laptopBag);
   return (
-    <div className="row row-cols-1 row-cols-md-4 g-4 container-fluid">
-      <div className="col">
-        <div className="card h-100">
-          <img
-            // style={{ height: "300px", width: "100%" }}
-            src=""
-            className="card-img-top img-fluid"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">Last updated 3 mins ago</small>
+    <div className="row row-cols-1 row-cols-md-3 g-4 container-fluid">
+      {laptopBags.map((singleLaptopBag) => (
+        <div className="col">
+          <div className="card h-100">
+            <img
+              // style={{ height: "300px", width: "100%" }}
+              src={singleLaptopBag.img}
+              className="card-img-top img-fluid"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+            </div>
+            <div className="card-footer">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
