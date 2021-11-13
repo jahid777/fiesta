31800f7e00c726dc6e07306eb5a833d143f9d201
unknown <za0162hid@gmail.com>
2021-11-13 15:22:39 +0600

    popup added

--- src/Components/Home/HomeComp/homeProduct.js
+++ src/Components/Home/HomeComp/homeProduct.js
@@ -7,6 +7,7 @@ import { anotherBagsData } from "../../../AllData/AllData";
 import { walletsData } from "../../../AllData/AllData";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import PopupModal from "../../Shared/PopupModal/PopupModal";
 
 const Bag = () => {
   const [shoes, setShoes] = useState(shoesData);
@@ -22,137 +23,160 @@ const Bag = () => {
   }, []);
 
   return (
-    <main className="mb-5 mt-2">
-      {/* this is for shoes */}
-      <div className="row row-cols-1 row-cols-md-4 g-4 container-fluid mt-2">
-        {shoes.map((singleShoes) => (
-          <div className="col">
-            <div className="card h-100">
-              <img
-                src={singleShoes?.img}
-                className="card-img-top img-fluid"
-                alt="..."
-                data-aos="fade-up"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-              </div>
-              <div className="card-footer">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-      {/* this is for bag */}
-      <div className="row row-cols-1 row-cols-md-4 g-4 container-fluid mt-5">
-        {bags.map((singleBag) => (
-          <div className="col">
-            <div className="card h-100">
-              <img
-                src={singleBag?.img}
-                className="card-img-top img-fluid"
-                alt="..."
-                data-aos="fade-up"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-              </div>
-              <div className="card-footer">
-                <small className="text-muted">Last updated 3 mins ago</small>
+    <main>
+      <div className="mb-5 mt-2">
+        <PopupModal />
+        {/* this is for shoes */}
+        <div className="row row-cols-1 row-cols-md-4 g-4 container-fluid mt-2">
+          {shoes.map((singleShoes) => (
+            <div className="col">
+              <div
+                className="card h-100"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
+                <img
+                  src={singleShoes?.img}
+                  className="card-img-top img-fluid"
+                  alt="..."
+                  data-aos="fade-up"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
-      {/* this is for belt */}
-      <div className="row row-cols-1 row-cols-md-4 g-4 container-fluid mt-5">
-        {belts.map((singleBelt) => (
-          <div className="col">
-            <div className="card h-100">
-              <img
-                src={singleBelt?.img}
-                className="card-img-top img-fluid"
-                alt="..."
-                data-aos="fade-up"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-              </div>
-              <div className="card-footer">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-      {/* another product(laptop, office) */}
-      <div className="row row-cols-1 row-cols-md-4 g-4 container-fluid mt-5">
-        {anotherBagData.map((singleBag) => (
-          <div className="col">
-            <div className="card h-100">
-              <img
-                src={singleBag?.img}
-                className="card-img-top img-fluid"
-                alt="..."
-                data-aos="fade-up"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-              </div>
-              <div className="card-footer">
-                <small className="text-muted">Last updated 3 mins ago</small>
+          ))}
+        </div>
+        {/* this is for bag */}
+        <div className="row row-cols-1 row-cols-md-4 g-4 container-fluid mt-5">
+          {bags.map((singleBag) => (
+            <div className="col">
+              <div
+                className="card h-100"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
+                <img
+                  src={singleBag?.img}
+                  className="card-img-top img-fluid"
+                  alt="..."
+                  data-aos="fade-up"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+        {/* this is for belt */}
+        <div className="row row-cols-1 row-cols-md-4 g-4 container-fluid mt-5">
+          {belts.map((singleBelt) => (
+            <div className="col">
+              <div
+                className="card h-100"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
+                <img
+                  src={singleBelt?.img}
+                  className="card-img-top img-fluid"
+                  alt="..."
+                  data-aos="fade-up"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        {/* another product(laptop, office) */}
+        <div className="row row-cols-1 row-cols-md-4 g-4 container-fluid mt-5">
+          {anotherBagData.map((singleBag) => (
+            <div className="col">
+              <div
+                className="card h-100"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
+                <img
+                  src={singleBag?.img}
+                  className="card-img-top img-fluid"
+                  alt="..."
+                  data-aos="fade-up"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
 
-      {/* this is for wallet */}
-      <div className="row row-cols-1 row-cols-md-4 g-4 container-fluid mt-5">
-        {wallets.map((singleWallet) => (
-          <div className="col">
-            <div className="card h-100">
-              <img
-                src={singleWallet?.img}
-                className="card-img-top img-fluid"
-                alt="..."
-                data-aos="fade-up"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-              </div>
-              <div className="card-footer">
-                <small className="text-muted">Last updated 3 mins ago</small>
+        {/* this is for wallet */}
+        <div className="row row-cols-1 row-cols-md-4 g-4 container-fluid mt-5">
+          {wallets.map((singleWallet) => (
+            <div className="col">
+              <div
+                className="card h-100"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
+                <img
+                  src={singleWallet?.img}
+                  className="card-img-top img-fluid"
+                  alt="..."
+                  data-aos="fade-up"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                </div>
+                <div className="card-footer">
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </main>
   );
---
+++ src/Components/Shared/PopupModal/WomenBagPopup.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+const WomenBagPopus = () => {
+  return (
+    <div>
+      <div>
+        <div
+          class="modal fade"
+          id="WomenBagModal"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">
+                  CONTACT US
+                </h5>
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="modal-body">
+                <h5>
+                  <div syle={{ display: "flex" }}>
+                    <i class="bi bi-telephone-outbound-fill"></i>{" "}
+                    <span style={{ marginLeft: "5px" }} className="pr-4">
+                      012200000000
+                    </span>
+                  </div>
+                </h5>
+                <h5>
+                  <div syle={{ display: "flex" }}>
+                    <i class="bi bi-envelope-fill"></i>
+                    <span style={{ marginLeft: "10px" }}>
+                      Example@gmail.com
+                    </span>
+                  </div>
+                </h5>
+              </div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WomenBagPopus;
