773219efd1d3a1083afd42131758b183137b1708
unknown <za0162hid@gmail.com>
2021-11-18 18:13:28 +0600

    gif adding

--- src/Components/Home/HomeComp/Gif.js
+++ src/Components/Home/HomeComp/Gif.js
@@ -4,11 +4,15 @@ import "./HomeComp.css";
 const Gif = () => {
   return (
     <div
-      style={{ display: "flex", justifyContent: "center", paddingTop: "40px" }}
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        paddingTop: "40px",
+      }}
     >
       <img
         src="https://i.imgur.com/NLA7Oym.gif"
-        title="source: imgur.com"
+        title=""
         alt="nai"
         className="gifImg"
       />
