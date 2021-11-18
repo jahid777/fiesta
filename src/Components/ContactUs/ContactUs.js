2b7a20516b21666fce01eeccac3d16fb41c937d8
unknown <za0162hid@gmail.com>
2021-11-18 17:34:46 +0600

    send message added

--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
 import "./ContactUs.css";
 
 const ContactUs = () => {
+  const [message, setMessage] = useState("");
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_j0pvscl",
+        "template_mi91flp",
+        e.target,
+        "user_BPAoo38Zr93sXfSxOt19S"
+      )
+      .then(
+        (result) => {
+          if (result) {
+            setMessage("your message successfully sent!");
+            setTimeout(() => {
+              setMessage("");
+            }, 5000);
+          }
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
   return (
     <div className="container my-5 py-3 contactMain">
       <div className="row">
@@ -30,7 +58,7 @@ const ContactUs = () => {
             style={{ width: "600px", height: "450px" }}
           ></iframe>
         </div>
-        <div className="col">
+        <form className="col" onSubmit={sendEmail}>
           <div className="form-group">
             <label htmlFor="inputName">Name</label>
             <input
@@ -38,16 +66,20 @@ const ContactUs = () => {
               className="form-control mt-2"
               id="inputName"
               placeholder="Your name"
+              name="name"
+              required
             />
           </div>
 
           <div className="form-group mt-4">
-            <label htmlFor="inputAddress">Address</label>
+            <label htmlFor="inputAddress">Phone Number</label>
             <input
-              type="text"
+              type="number"
               className="form-control mt-2"
               id="inputAddress"
               placeholder="Your address"
+              name="number"
+              required
             />
           </div>
           <div className="form-group mt-4">
@@ -57,6 +89,8 @@ const ContactUs = () => {
               className="form-control mt-2"
               id="inputEmail"
               placeholder="Your email"
+              name="email"
+              required
             />
           </div>
           <div className="form-group mt-4">
@@ -66,6 +100,8 @@ const ContactUs = () => {
               className="form-control mt-2"
               id="inputMessage"
               placeholder="Type your message........"
+              name="message"
+              required
             />
           </div>
 
@@ -74,7 +110,12 @@ const ContactUs = () => {
               Send
             </button>
           </div>
-        </div>
+          <div>
+            <p style={{ color: "green", fontWeight: "bold", marginTop: "5px" }}>
+              {message}
+            </p>
+          </div>
+        </form>
       </div>
     </div>
   );
