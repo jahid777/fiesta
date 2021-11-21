e013cf54c8ae6c30c92a08986cb147b3e6c52202
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2021-11-21 14:02:48 +0600

    random change

--- src/Components/Home/HomeComp/Carousal.js
+++ src/Components/Home/HomeComp/Carousal.js
@@ -13,7 +13,7 @@ const Bag = () => {
           type="button"
           data-bs-target="#carouselExampleIndicators"
           data-bs-slide-to="0"
-          className="active"
+          className="active carouselIndicators"
           aria-current="true"
           aria-label="Slide 1"
         ></button>
@@ -22,12 +22,14 @@ const Bag = () => {
           data-bs-target="#carouselExampleIndicators"
           data-bs-slide-to="1"
           aria-label="Slide 2"
+          className="carouselIndicators"
         ></button>
         <button
           type="button"
           data-bs-target="#carouselExampleIndicators"
           data-bs-slide-to="2"
           aria-label="Slide 3"
+          className="carouselIndicators"
         ></button>
       </div>
       <div className="carousel-inner">
